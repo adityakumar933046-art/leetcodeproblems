2c23bdd0b3e66446efc1e91f7074cee6cecaffca
ADITYA KUMAR <adityakumar933046@gmail.com>
2026-06-23 14:12:14 +0530

    Time: 0 ms (100%), Space: 19.3 MB (64.91%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -1,8 +1,6 @@
 class Solution:
     def nextPermutation(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
+
         n = len(nums)
 
         i = n - 2
