fdddba496c5cd9137241bee0afa5b643f16e6e50
ADITYA KUMAR <adityakumar933046@gmail.com>
2026-06-23 15:19:21 +0530

    Time: 1412 ms (5.03%), Space: 20.4 MB (74.95%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.py
@@ -0,0 +1,8 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        for i in range(max(nums)+1):
+            if i not in nums:
+                return i
+        return max(nums)+1
+
+        
